181e210f73961f938e2d0b3436f13c03677e6de5
Rahultadmybrand <rahul.thakur@admybrand.com>
2021-09-05 17:52:38 +0530

    added testimonials

--- src/components/TestimonialCard.js
+++ src/components/TestimonialCard.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
-const TestimonialCard = () => {
+const TestimonialCard = ({data}) => {
     return (
-        <div>
-            <img src="https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png?w=640"/>
-            <h1>
-                Name
-            </h1>
-            <p>
-                Quote jfnweucehiungu wechweucyegyiuvewiychuibjewqihnbhcisokjbhcbasioncbk skiconbksisabkhcjas
+        <div className="flex flex-col border bg-red-500 shadow-md  border-red-600 rounded">
+            {/* <img src="https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png?w=640"/> */}
+            <div className="flex flex-col flex-wrap justify-start items-start p-8">
+            <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="#fff" className="text-center md:text-left w-full"><path d="M9.983 3v7.391c0 5.704-3.731 9.57-8.983 10.609l-.995-2.151c2.432-.917 3.995-3.638 3.995-5.849h-4v-10h9.983zm14.017 0v7.391c0 5.704-3.748 9.571-9 10.609l-.996-2.151c2.433-.917 3.996-3.638 3.996-5.849h-3.983v-10h9.983z"/></svg>
+            <p className="text-left tracking-tight  text-white leading-normal mt-2">
+                {data.quote}
             </p>
+            <span className="text-base font-semibold text-left text-white bg-red-600 rounded p-1">
+               - {data.name}
+            </span>
+            </div>
         </div>
     );
 }
--- src/components/Testimonies.js
+++ src/components/Testimonies.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import TestimonialCard from './TestimonialCard';
-import {quoteData} from "../datafactory/quoteData.json";
+import {committeeHeadData} from "../datafactory/committeeHeadData.json";
 
 const Testimonies = () => {
     return (
@@ -9,12 +9,9 @@ const Testimonies = () => {
             <h1 className="text-2xl lg:text-5xl font-semibold text-black flex flex-wrap justify-center font-extrabold">
               Testimonials
             </h1>
-            <p className="text-gray-700 mt-2 text-base md:text-lg">
-              Some words from our club members
-            </p>
-            <div className="grid grid-cols-1 place-items-center sm:place-items-start sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-4 gap-8">
+            <div className="grid grid-cols-1  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-4 gap-4">
              {
-               quoteData.map(item=><TestimonialCard/>)
+               committeeHeadData.map(item=><TestimonialCard data={item}/>)
              }
             </div>
           </div>
